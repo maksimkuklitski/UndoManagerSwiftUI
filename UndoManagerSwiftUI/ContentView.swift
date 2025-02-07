2a35cdbe3e5cd338bd2b2fddf641e624bb6b0a0c
Maksim Kuklitski <maksim.kuklitski@innowise.com>
2025-02-07 08:27:57 +0100

    Initial Commit

---
+++ UndoManagerSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UndoManagerSwiftUI
+//
+//  Created by Maksim Kuklitski on 07/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UndoManagerSwiftUI/UndoManagerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UndoManagerSwiftUIApp.swift
+//  UndoManagerSwiftUI
+//
+//  Created by Maksim Kuklitski on 07/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct UndoManagerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
